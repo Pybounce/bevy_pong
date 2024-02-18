59c4f434d4d04d201591898c90ce4e911e981f5b
Pybounce <pybounce@protonmail.com>
2024-02-18 12:16:53 +0000

    moving paddle creation to it's own plugin

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -1,36 +1,21 @@
+mod plugins;
+use plugins::paddles::*;
+
 use bevy::{
     prelude::*, 
     window::close_on_esc,
 };
 
+
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_systems(PreStartup, setup_game_config)
-    .add_systems(Startup, (spawn_paddles, spawn_camera))
+    .add_plugins((DefaultPlugins, PaddlesPlugin))
+    .add_systems(Startup, spawn_camera)
     .add_systems(Update, close_on_esc)
     .run();
 }
 
-fn spawn_paddles(mut commands: Commands, game_config: Res<GameConfig>) {
-    spawn_paddle(&mut commands, &game_config.left_paddle_config);
-    spawn_paddle(&mut commands, &game_config.right_paddle_config);
-}
 
-fn spawn_paddle(commands: &mut Commands, paddle_config: &PaddleConfig) {
-    commands.spawn(SpriteBundle {
-        transform: Transform {
-            translation: paddle_config.position.extend(0.0),
-            scale: paddle_config.size.extend(1.0),
-            ..Default::default()
-        },
-        sprite: Sprite {
-            color: paddle_config.colour.into(),
-            ..default()
-        },
-        ..default()
-    });
-}
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
@@ -40,35 +25,3 @@ fn spawn_camera(mut commands: Commands) {
 
 
 
-fn setup_game_config(mut commands: Commands) {
-    let mut game_config = GameConfig::default();
-    game_config.left_paddle_config.position.x = -500.0;
-    game_config.left_paddle_config.colour = Vec4::new(0.7, 0.0, 0.0, 1.0);
-    game_config.right_paddle_config.position.x = 500.0;
-    game_config.right_paddle_config.colour = Vec4::new(0.0, 0.0, 0.7, 1.0);
-
-    commands.insert_resource(game_config);
-}
-
-
-
-#[derive(Resource, Default)]
-struct GameConfig
-{
-    left_paddle_config: PaddleConfig,
-    right_paddle_config: PaddleConfig
-}
-struct PaddleConfig {
-    colour: Vec4,
-    size: Vec2,
-    position: Vec2
-}
-impl Default for PaddleConfig {
-    fn default() -> Self {
-        Self {
-            colour: Vec4::new(1.0, 1.0, 1.0, 1.0),
-            size: Vec2::new(15.0, 100.0),
-            position: Vec2::new(0.0, 0.0)
-        }
-    }
-}
---
+++ pong/src/plugins/paddles.rs
@@ -0,0 +1,69 @@
+
+use bevy::prelude::*;
+
+pub struct PaddlesPlugin;
+
+impl Plugin for PaddlesPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PreStartup, setup_paddles_config)
+        .add_systems(Startup, spawn_paddles);
+    }
+}
+#[derive(Component)]
+pub struct LeftPaddle;
+#[derive(Component)]
+pub struct RightPaddle;
+
+#[derive(Resource, Default)]
+struct PaddlesConfig
+{
+    l_paddle: PaddleConfig,
+    r_paddle: PaddleConfig
+}
+
+struct PaddleConfig {
+    colour: Vec4,
+    size: Vec2,
+    position: Vec2
+}
+
+impl Default for PaddleConfig {
+    fn default() -> Self {
+        Self {
+            colour: Vec4::new(1.0, 1.0, 1.0, 1.0),
+            size: Vec2::new(15.0, 100.0),
+            position: Vec2::new(0.0, 0.0)
+        }
+    }
+}
+
+fn spawn_paddles(mut commands: Commands, game_config: Res<PaddlesConfig>) {
+    spawn_paddle(&mut commands, &game_config.l_paddle, LeftPaddle);
+    spawn_paddle(&mut commands, &game_config.r_paddle, RightPaddle);
+}
+
+fn spawn_paddle<T: Component>(commands: &mut Commands, paddle_config: &PaddleConfig, paddle_component: T) {
+    commands.spawn((paddle_component, SpriteBundle {
+        transform: Transform {
+            translation: paddle_config.position.extend(0.0),
+            scale: paddle_config.size.extend(1.0),
+            ..default()
+        },
+        sprite: Sprite {
+            color: paddle_config.colour.into(),
+            ..default()
+        },
+        ..default()
+    }));
+}
+
+fn setup_paddles_config(mut commands: Commands) {
+    let mut paddles_config = PaddlesConfig::default();
+    paddles_config.l_paddle.position.x = -500.0;
+    paddles_config.l_paddle.colour = Vec4::new(0.7, 0.0, 0.0, 1.0);
+    paddles_config.r_paddle.position.x = 500.0;
+    paddles_config.r_paddle.colour = Vec4::new(0.0, 0.0, 0.7, 1.0);
+
+    commands.insert_resource(paddles_config);
+}
+
